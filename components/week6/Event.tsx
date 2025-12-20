60849b088afb5ea79ce43e77d1b98988f60ded0f
Boyeiei9 <151432874+Boyeiei9@users.noreply.github.com>
2025-12-20 13:12:29 +0700

    Update Week6

--- components/week6/Event.tsx
+++ components/week6/Event.tsx
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from "react";
 import { FlatList, Image, Text, View } from "react-native";
 
 export default function Event(props: any) {
-    const [onlineTours, setOnlineTours] = useState([]);
+    const [onlineTours, setOnlineTours] = useState<any[]>([]);
+
     const loadOnlineTours = async () => {
         try {
-            let text = await fetch('https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/json/events.json');
+            let text = await fetch(
+                "https://raw.githubusercontent.com/arc6828/myreactnative/master/assets/json/events.json"
+            );
             let data = await text.json();
-            console.log("Load Data : ", data);
-            //SET STATE
             setOnlineTours(data);
         } catch (error) {
             console.log("ERROR : ", error);
         }
-    }
+    };
 
     useEffect(() => {
         loadOnlineTours();
@@ -21,30 +22,41 @@ export default function Event(props: any) {
 
     return (
         <View style={props.style}>
-            <Text style={{ fontSize: 20 }}>Upcoming Event</Text>
-            <Text style={{ color: "grey" }}>Let find out what most interesting things</Text>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                Upcoming Event
+            </Text>
+            <Text style={{ color: "grey", marginBottom: 10 }}>
+                Let find out what most interesting things
+            </Text>
+
             <FlatList
-                horizontal={true}
+                horizontal
+                showsHorizontalScrollIndicator={false}
                 data={onlineTours}
-                renderItem={
-                    ({ item, index }) => {
-                        console.log(item, index, item.uri);
-                        return (
-                            <View style={{ marginRight: 10 }}>
-                                <Image style={{ width: 200, height: 150, borderRadius: 10, borderTopRightRadius: 10 }} source={{ uri: item.uri }} />
-                                <View style={{ flexDirection: "row", marginRight: 10,borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}>
-                                    <View style={{  marginTop : -5,height: 30, width: 200, paddingHorizontal: 10, backgroundColor: 'black', opacity: 0.5, borderBottomLeftRadius: 10, borderBottomRightRadius: 10, }}>
-                                        <Text style={{ fontSize: 20, color: "white" }}>Tour in Somewhere</Text>
-                                    </View>
+                keyExtractor={(item: any) => item.id}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={{ marginRight: 15, width: 220 }}>
+                            <Image
+                                source={{ uri: item.uri }}
+                                style={{ width: "100%", height: 140, borderTopLeftRadius: 12, borderTopRightRadius: 12, }} />
+                            <View
+                                style={{ flexDirection: "row", borderWidth: 1, borderColor: "#ddd", borderBottomLeftRadius: 12, borderBottomRightRadius: 12, backgroundColor: "#fff", }}>
+                                <View
+                                    style={{ padding: 10, width: 60, alignItems: "center", justifyContent: "center", borderRightWidth: 1, borderRightColor: "#eee", }}>
+                                    <Text style={{ fontSize: 12, color: "red", fontWeight: "bold", }}>DEC</Text>
+                                    <Text style={{ fontSize: 18, fontWeight: "bold", }}>{item.date}</Text>
+                                </View>
+                                <View style={{ padding: 10, flex: 1 }}>
+                                    <Text style={{ fontSize: 14, fontWeight: "bold", }} numberOfLines={1}>{item.title}</Text>
+                                    <Text style={{ fontSize: 12, color: "grey" }}>{item.time}</Text>
+                                    <Text style={{ fontSize: 12, color: "grey" }}>{item.place}</Text>
                                 </View>
                             </View>
-                        );
-                    }
-                }
-                keyExtractor={(item: any) => item.id}
+                        </View>
+                    );
+                }}
             />
         </View>
     );
 }
-
-
